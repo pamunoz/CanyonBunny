e5942f40dc6928583cfec7b05909b8a74271fc06
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-03 15:42:30 -0400

    add implementation for methods in CanyonBunnyMain

--- desktop/src/com/pfariasmunoz/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/pfariasmunoz/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 480;
+		config.title = "Canyon Bunny";
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
