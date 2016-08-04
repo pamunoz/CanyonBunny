14f328dc16895f3b38b55c43f5a31465a8cf96cf
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-04 15:10:27 -0400

    add the wateroverlay object

--- core/src/com/pfariasmunoz/libgdx/canyonbunny/game/objects/WaterOverlay.java
+++ core/src/com/pfariasmunoz/libgdx/canyonbunny/game/objects/WaterOverlay.java
@@ -1,7 +1,31 @@
 package com.pfariasmunoz.libgdx.canyonbunny.game.objects;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.pfariasmunoz.libgdx.canyonbunny.game.Assets;
 /**
  * Created by Pablo Farias on 04-08-16.
  */
-public class WaterOverlay {
+public class WaterOverlay extends AbstractGameObject {
+    private TextureRegion regWaterOverlay;
+    private float length;
+
+    public WaterOverlay(float length) {
+        this.length = length;
+        init();
+    }
+
+    private void init() {
+        dimension.set(length * 10, 3);
+
+        regWaterOverlay = Assets.instance.levelDecoration.waterOverlay;
+        origin.x = -dimension.x / 2;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        TextureRegion reg = null;
+        reg = regWaterOverlay;
+        batch.draw(reg.getTexture(), position.x + origin.x, position.y + origin.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+    }
 }
