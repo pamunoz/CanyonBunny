6664f6a08a92f30cf92ef4d80dc541e39008b1f2
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-04 18:14:14 -0400

    add second camera for the game GUI

--- core/src/com/pfariasmunoz/libgdx/canyonbunny/game/WorldRenderer.java
+++ core/src/com/pfariasmunoz/libgdx/canyonbunny/game/WorldRenderer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 import com.pfariasmunoz.libgdx.canyonbunny.util.Constants;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 /**
  * Created by Pablo Farias on 03-08-16.
@@ -12,6 +13,7 @@ import com.pfariasmunoz.libgdx.canyonbunny.util.Constants;
 public class WorldRenderer implements Disposable {
 
     private OrthographicCamera camera;
+    private OrthographicCamera cameraGUI;
     private SpriteBatch batch;
     private WorldController worldController;
 
@@ -25,6 +27,10 @@ public class WorldRenderer implements Disposable {
         camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
         camera.position.set(0, 0, 0);
         camera.update();
+        cameraGUI = new OrthographicCamera(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT);
+        cameraGUI.position.set(0, 0, 0);
+        cameraGUI.setToOrtho(true); // flip y-axis
+        cameraGUI.update();
     }
 
     public void render() {
@@ -32,8 +38,12 @@ public class WorldRenderer implements Disposable {
     }
 
     public void resize(int width, int height) {
-        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / (float) height) * (float) width;
         camera.update();
+        cameraGUI.viewportHeight = Constants.VIEWPORT_GUI_HEIGHT;
+        cameraGUI.viewportWidth = (Constants.VIEWPORT_GUI_HEIGHT / (float) height * (float) width);
+        cameraGUI.position.set(cameraGUI.viewportWidth / 2, cameraGUI.viewportHeight / 2, 0);
+        cameraGUI.update();
     }
 
     @Override
