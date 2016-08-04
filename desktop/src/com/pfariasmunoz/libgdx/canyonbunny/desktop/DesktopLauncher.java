d72df9589b2af7dbc7701afed6690894ab61056b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-04 01:07:44 -0400

    add Assets class and implementation

--- desktop/src/com/pfariasmunoz/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/pfariasmunoz/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -5,11 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pfariasmunoz.libgdx.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Canyon Bunny";
 		config.width = 800;
 		config.height = 480;
-		config.title = "Canyon Bunny";
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
