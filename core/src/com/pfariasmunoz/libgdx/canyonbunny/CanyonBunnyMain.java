4af65e878af6a4ccb7a90b0de5e1eb2af8647b6d
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-07 20:13:02 -0400

    add refactoring to CanyonBunnyMain

--- core/src/com/pfariasmunoz/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/pfariasmunoz/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,70 +1,21 @@
 package com.pfariasmunoz.libgdx.canyonbunny;
 
 import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.GL20;
 import com.pfariasmunoz.libgdx.canyonbunny.game.Assets;
-import com.pfariasmunoz.libgdx.canyonbunny.game.WorldController;
-import com.pfariasmunoz.libgdx.canyonbunny.game.WorldRenderer;
+import com.pfariasmunoz.libgdx.canyonbunny.screens.MenuScreen;
 
-public class CanyonBunnyMain implements ApplicationListener {
+public class CanyonBunnyMain extends Game {
 
-	private static final String TAG = CanyonBunnyMain.class.getName();
-
-	private WorldController worldController;
-	private WorldRenderer worldRenderer;
-
-	private boolean paused;
-
-	@Override
-	public void create () {
-		// Set Libgdx log level to DEBUG
-		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-		// Load assets
-		Assets.instance.init(new AssetManager());
-		// Initialize controller and renderer
-		worldController = new WorldController();
-		worldRenderer = new WorldRenderer(worldController);
-	}
-
-	@Override
-	public void render () {
-		// Do not update game world when paused.
-		if (!paused) {
-			// Update game world by the time that has passed
-			// since last rendered frame.
-			worldController.update(Gdx.graphics.getDeltaTime());
-		}
-		// Sets the clear screen color to: Cornflower Blue
-		Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
-		// Clears the screen
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// Render game world to screen
-		worldRenderer.render();
-	}
-
-	@Override
-	public void resize (int width, int height) {
-		worldRenderer.resize(width, height);
-	}
-
-	@Override
-	public void pause () {
-		paused = true;
-	}
-
-	@Override
-	public void resume () {
-		Assets.instance.init(new AssetManager());
-		paused = false;
-	}
-
-	@Override
-	public void dispose () {
-		worldRenderer.dispose();
-		Assets.instance.dispose();
-	}
-
+    @Override
+    public void create() {
+        // Set Libgdx log level
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Load assets
+        Assets.instance.init(new AssetManager());
+        // Start game at menu screen
+        setScreen(new MenuScreen(this));
+    }
 }
